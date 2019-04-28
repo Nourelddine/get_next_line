0b927adc0c3d570be174bb8f32091c6488fb52e9
Noureddine Abdelbadie <nabdelba@e2r13p4.1337.ma>
2019-04-28 01:00:58 +0100

    Basic tests Passed !

--- get_next_line.c
+++ get_next_line.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nabdelba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nabdelba <nabdelba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/24 17:13:38 by nabdelba          #+#    #+#             */
-/*   Updated: 2019/04/24 17:13:38 by nabdelba         ###   ########.fr       */
+/*   Created: 2019/04/28 00:58:45 by nabdelba          #+#    #+#             */
+/*   Updated: 2019/04/28 00:59:40 by nabdelba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./get_next_line.h"
 
-int find_char(const char *s, char c)
+int		find_char(const char *s, char c)
 {
 	int i;
 
@@ -28,34 +28,16 @@ int find_char(const char *s, char c)
 	return (0);
 }
 
-/*int        find_char(char *s, char c)
+int		get_next_line(const int fd, char **line)
 {
-    int     i;
-
-    i = 0;
-    if (s == NULL)
-        return (0);
-    while (s[i])
-    {
-        if (s[i] == c)
-            return (i);
-		i++;
-    }
-    return (0);
-}*/
-
-int get_next_line(const int fd, char **line)
-{
-	int sz;
-	char buffer[BUFF_SIZE + 1];
-	static t_list *file;
-	t_list *tmp;
-	int i;
+	int		sz;
+	int		i;
+	char	buffer[BUFF_SIZE + 1];
+	static	t_list *file;
+	t_list	*tmp;
 
 	if ((read(fd, buffer, 0) < 0 || fd < 0 || !line))
-	{
 		return (-1);
-	}
 	tmp = (t_list *)ft_memalloc(sizeof(t_list));
 	tmp->content = ft_strnew(0);
 	ft_lstinsert(&file, tmp);
@@ -63,30 +45,20 @@ int get_next_line(const int fd, char **line)
 	{
 		buffer[sz] = '\0';
 		if (!(file->content = ft_strjoin(file->content, buffer)))
-		{
 			return (-1);
-		}
 		if ((ft_strchr(buffer, '\n')))
-		{
-			break;
-		}
+			break ;
 	}
 	if (sz == 0 && !ft_strlen((char *)file->content))
 		return (0);
+	if (!ft_strchr(file->content, '\n'))
+	{
+		*line = ft_strdup(file->content);
+		ft_strclr(file->content);
+		return (1);
+	}
 	i = find_char(file->content, '\n');
 	*line = ft_strsub(file->content, 0, i);
 	file->content = ft_strchr(file->content, '\n') + 1;
 	return (1);
 }
-
-// int main()
-// {
-// 	int fd;
-// 	char *line;
-// 	fd = open("cmp.txt",O_RDONLY);
-// 	while (get_next_line(fd,&line))
-// 	{
-// 		printf("%s",line);
-// 		free(line);
-// 	}
-// }
--- main.c
+++ main.c
@@ -1,10 +1,2 @@
 #include "./get_next_line.h"
 
-int main()
-{
-	char buffer[BUFF_SIZE];
-	int fd = open("test.opt",O_RDONLY);
-	int red = read(fd,buffer,BUFF_SIZE);
-	buffer[red] = '\n';
-	printf("%s",buffer);
-}
