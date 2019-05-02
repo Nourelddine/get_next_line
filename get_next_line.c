7ba04b88d7d404fceaa576562026b8b0ec877a22
Noureddine Abdelbadie <nabdelba@e2r8p1.1337.ma>
2019-05-02 23:01:03 +0100

    <Fixing Memory Leaks>

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,11 @@
 /*   By: nabdelba <nabdelba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 00:58:45 by nabdelba          #+#    #+#             */
-/*   Updated: 2019/04/30 01:09:59 by nabdelba         ###   ########.fr       */
+/*   Updated: 2019/05/02 22:56:21 by nabdelba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./get_next_line.h"
-
-static int		find_char(const char *s, char c)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0' && s[i] != c)
-		i++;
-	return ((s[i] == c) ? i : 0);
-}
 
 static t_list	*handle_file(t_list **file, int fd)
 {
@@ -33,28 +23,41 @@ static t_list	*handle_file(t_list **file, int fd)
 			return (p);
 		p = p->next;
 	}
-	p = ft_lstnew("\0", fd);
+	p = malloc(sizeof(t_list));
+	p->content = ft_strnew(0);
+	p->content_size = fd;
 	ft_lstadd(file, p);
 	p = *file;
 	return (p);
 }
 
+static void		read_file(t_list **node, int fd, int *sz, char **tmp)
+{
+	char	buffer[BUFF_SIZE];
+
+	while ((*sz = read(fd, buffer, BUFF_SIZE)) && *sz)
+	{
+		buffer[*sz] = '\0';
+		*tmp = ft_strnew(ft_strlen((char *)(*node)->content));
+		ft_strcpy(*tmp, (*node)->content);
+		(*node)->content = ft_strjoin(*tmp, buffer);
+		free(*tmp);
+		if ((ft_strchr((*node)->content, '\n')))
+			break ;
+	}
+}
+
 int				get_next_line(const int fd, char **line)
 {
 	static t_list	*file;
-	char			buffer[BUFF_SIZE + 1];
 	int				sz;
 	t_list			*node;
+	char			*tmp;
 
+	sz = 0;
 	RETURN_IF((read(fd, NULL, 0) < 0 || fd < 0 || !line), -1);
 	node = handle_file(&file, fd);
-	while ((sz = read(fd, buffer, BUFF_SIZE)) && sz)
-	{
-		buffer[sz] = '\0';
-		node->content = ft_strjoin(node->content, buffer);
-		if ((ft_strchr(node->content, '\n')))
-			break ;
-	}
+	read_file(&node, fd, &sz, &tmp);
 	RETURN_IF((sz == 0 && !ft_strlen((char *)node->content)), 0);
 	if (!ft_strchr(node->content, '\n'))
 	{
@@ -62,7 +65,11 @@ int				get_next_line(const int fd, char **line)
 		ft_strclr(file->content);
 		return (1);
 	}
-	*line = ft_strsub(node->content, 0, find_char(node->content, '\n'));
-	node->content = ft_strchr(node->content, '\n') + 1;
+	*line = ft_strsub(node->content, 0, ft_findchr(node->content, '\n'));
+	tmp = ft_strnew(ft_strlen((char *)node->content));
+	ft_strcpy(tmp, node->content);
+	free(node->content);
+	node->content = ft_strdup(ft_strchr(tmp, '\n') + 1);
+	free(tmp);
 	return (1);
 }
