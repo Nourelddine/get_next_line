b3db895fb813d6f79fdb631d8ec20468f2a53908
Noureddine Abdelbadie <nabdelba@e2r9p9.1337.ma>
2019-06-29 03:19:07 +0100

    Final v : By Aimane

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: nabdelba <nabdelba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 01:10:39 by nabdelba          #+#    #+#             */
-/*   Updated: 2019/06/28 09:21:13 by nabdelba         ###   ########.fr       */
+/*   Updated: 2019/06/29 03:15:42 by nabdelba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "./libft/libft.h"
 # include <fcntl.h>
 # include <unistd.h>
-# define BUFF_SIZE 32
+# define BUFF_SIZE 16
 
 # define RETURN_IF(x, ret) if (x) return (ret);
 
